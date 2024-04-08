c24e8657acd00bdebcfe7aa8151092d63e8706f0
Paul Turpie <3794399+turpie@users.noreply.github.com>
2024-04-08 12:41:07 +1000

    Change filenames of Queue files to the hash of the original file's name so that if it is re-queued a second time, it won't create a duplicate queue entry.

--- AddToHandbrakeQueue.ps1
+++ AddToHandbrakeQueue.ps1
@@ -8,8 +8,9 @@ $FilePath | ForEach-Object{
     $FileToQueue = $_
     Write-Host "Queueing:" $FileToQueue
     #$FileToQueue | Out-File -FilePath (Join-Path -Path $QueuePath -ChildPath (Split-Path -Path $FileToQueue -Leaf))
-    # Filenames were containing wildcards that Out-File complained about, using GUID for filename instead.
-    $FileToQueue | Out-File -FilePath (Join-Path -Path $QueuePath -ChildPath (New-Guid))
+    # Filenames were containing wildcards that Out-File complained about, using hash of filename instead.
+    $HashedFilename = (Get-FileHash -InputStream ([IO.MemoryStream]::new([byte[]][char[]]$FileToQueue)) -Algorithm SHA256).Hash
+    $FileToQueue | Out-File -FilePath (Join-Path -Path $QueuePath -ChildPath $HashedFilename)
 }
 
-#Pause 
+Pause 
