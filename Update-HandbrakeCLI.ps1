50adf82a66a65e7ec6ce689ad2d7712d42797ca7
Paul Turpie <3794399+turpie@users.noreply.github.com>
2024-12-19 10:27:33 +1100

    refactored to handle new installations where the CLI doesn't already exist. and to add some error checking

--- Update-HandbrakeCLI.ps1
+++ Update-HandbrakeCLI.ps1
@@ -7,29 +7,52 @@ if ($PSVersionTable.PSVersion -lt [System.Version]"7.0.0") {
     $ProgressPreference = 'SilentlyContinue'
 }
 
-Write-Host "Checking that HandbrakeCLI up-to-date ......"
+function Install-HandBrakeCLI {
+    param (
+        [string]$downloadURL
+    )
+    Write-Host "Downloading HandbrakeCLI from $downloadURL"
+    Invoke-WebRequest -Uri $downloadURL -OutFile $tempFile
+    Expand-Archive -Path $tempFile -DestinationPath (Split-Path $HandBrakeExe) -Force
+    # Clean up the temporary file
+    Remove-Item -Path $tempFile -Force
 
+    $localVersion = [System.Version]((&$HandBrakeExe --version)[0].Split(" ")[1])
+    Write-Host "HandbrakeCLI version $localVersion has been installed."
+}
 
-$localVersion = [System.Version]((&$HandBrakeExe --version)[0]).Split(" ")[1]
-Write-Host "Currently installed HandbrakeCLI version is $localVersion"
+Write-Host "Checking that HandbrakeCLI is up-to-date ......"
 
 $remoteResult = Invoke-RestMethod -Uri $HandbrakeReleaseURI
 $remoteVersion = [System.Version]$remoteResult.name
 Write-Host "Remote Github HandbrakeCLI version is $remoteVersion"
+# Find the download URL.
+$downloadURL = ($remoteResult.assets | Where-Object name -Like "*CLI*win-x86_64.zip").browser_download_url
 
-if ($localVersion -lt $remoteVersion) {
-    Write-Host "HandbrakeCLI is out of date. Getting $remoteVersion"
-    #Backup the old version, and rename to include the version number in it's filename
-    Move-Item -Path $HandBrakeExe -Destination ($HandBrakeExe.Replace("CLI.exe", "CLI-" + ([string]$localVersion) + ".exe"))
-
-    # Find the download URL.
-    $downloadURL = ($remoteResult.assets | Where-Object name -Like "*CLI*win-x86_64.zip").browser_download_url
-    Invoke-WebRequest -Uri $downloadURL -OutFile $tempFile
-    Expand-Archive -Path $tempFile -Force
-
-    $localVersion = [System.Version]((&$HandBrakeExe --version)[0]).Split(" ")[1]
-    Write-Host "HandbrakeCLI has been updated to $localVersion"
+if (-Not (Test-Path $HandBrakeExe)) {
+    Write-Host "HandBrakeCLI executable not found at $HandBrakeExe"
+    Install-HandBrakeCLI -downloadURL $downloadURL
+    exit 1
 }
 else {
-    Write-Host "HandbrakeCLI is up to date."
+    try {
+        $localVersion = [System.Version]((&$HandBrakeExe --version)[0].Split(" ")[1])
+        Write-Host "Currently installed HandbrakeCLI version is $localVersion"
+    
+        if ($localVersion -lt $remoteVersion) {
+            Write-Host "HandbrakeCLI is out of date. Getting $remoteVersion"
+            # Backup the old version, and rename to include the version number in its filename
+            Move-Item -Path $HandBrakeExe -Destination ($HandBrakeExe.Replace("CLI.exe", "CLI-" + ([string]$localVersion) + ".exe"))
+    
+            Install-HandBrakeCLI -downloadURL $downloadURL
+        }
+        else {
+            Write-Host "HandbrakeCLI is up to date."
+        }
+    }
+    catch {
+        Write-Host "An error occurred: $_"
+    
+    }
 }
+
