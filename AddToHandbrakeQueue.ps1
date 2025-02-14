9d0b28077b9146644cb83c4325025a4686a0ebcc
turpie <3794399+turpie@users.noreply.github.com>
2025-02-14 13:42:37 +1100

    Updated AddToHandbrakeQueue.ps1 to allow adding folders as well as files. Folders will be recursively searched for files matching common video filename extentions. (e.g. .mov .mp4 .avi)

--- AddToHandbrakeQueue.ps1
+++ AddToHandbrakeQueue.ps1
@@ -2,13 +2,38 @@ param(
     [Parameter(ValueFromRemainingArguments = $true)]
     $FilePath
 )
+# The script is designed to be called from the context menu of a file or folder. It will queue the file or all video files in the folder for processing by Handbrake. 
+
+
 $QueuePath = "C:\Scripts\HandBrakeBatcher\Queue"
 
-$FilePath | ForEach-Object{
-    $FileToQueue = $_
+$FilePath
+
+function Queue-File {
+    param (
+        #filename to queue
+        [string]$FileToQueue
+    )
     Write-Host "Queueing:" $FileToQueue
     #$FileToQueue | Out-File -FilePath (Join-Path -Path $QueuePath -ChildPath (Split-Path -Path $FileToQueue -Leaf))
     # Filenames were containing wildcards that Out-File complained about, using hash of filename instead.
     $HashedFilename = (Get-FileHash -InputStream ([IO.MemoryStream]::new([byte[]][char[]]$FileToQueue)) -Algorithm SHA256).Hash
     $FileToQueue | Out-File -FilePath (Join-Path -Path $QueuePath -ChildPath $HashedFilename)
+    
 }
+
+$videoExtensions = @(".mp4", ".mkv", ".avi", ".mov", ".wmv")      
+$FilePath | ForEach-Object{
+    # if the file is a directory, queue all files in the directory
+    if (Test-Path -Path $_ -PathType Container) {
+        # only queue video files
+        Get-ChildItem -Path $_ -Recurse -File | Where-Object { $videoExtensions -contains $_.Extension } | ForEach-Object {
+            Queue-File -FileToQueue $_.FullName
+        }
+    }
+    else {
+        if ($videoExtensions -contains (Get-Item $_).Extension) {
+            Queue-File -FileToQueue $_
+        }
+    }
+} 
